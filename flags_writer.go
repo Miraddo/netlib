789eab807da77cec3212b3607eeaf82861e53e7a
Milad Poshtdari <mposhtdari@gmail.com>
2022-05-05 08:45:28 +0430

    style: change some style of the code

--- flags_writer.go
+++ flags_writer.go
@@ -1,49 +1,14 @@
 package tcp
 
-func (p Packet) SetFlagReserved1() {
-	p[12] |= FlagReserved1
-}
-
-func (p Packet) SetFlagReserved2() {
-	p[12] |= FlagReserved2
-}
-
-func (p Packet) SetFlagReserved3() {
-	p[12] |= FlagReserved3
-}
-
-func (p Packet) SetFlagNS() {
-	p[12] |= FlagNS
-}
-
-func (p Packet) SetFlagCWR() {
-	p[13] |= FlagCWR
-}
-
-func (p Packet) SetFlagECE() {
-	p[13] |= FlagECE
-}
-
-func (p Packet) SetFlagURG() {
-	p[13] |= FlagURG
-}
-
-func (p Packet) SetFlagACK() {
-	p[13] |= FlagACK
-}
-
-func (p Packet) SetFlagPSH() {
-	p[13] |= FlagPSH
-}
-
-func (p Packet) SetFlagRST() {
-	p[13] |= FlagRST
-}
-
-func (p Packet) SetFlagSYN() {
-	p[13] |= FlagSYN
-}
-
-func (p Packet) SetFlagFIN() {
-	p[13] |= FlagFIN
-}
+func (p Packet) SetFlagReserved1() { p[12] |= FlagReserved1 }
+func (p Packet) SetFlagReserved2() { p[12] |= FlagReserved2 }
+func (p Packet) SetFlagReserved3() { p[12] |= FlagReserved3 }
+func (p Packet) SetFlagNS()        { p[12] |= FlagNS }
+func (p Packet) SetFlagCWR()       { p[13] |= FlagCWR }
+func (p Packet) SetFlagECE()       { p[13] |= FlagECE }
+func (p Packet) SetFlagURG()       { p[13] |= FlagURG }
+func (p Packet) SetFlagACK()       { p[13] |= FlagACK }
+func (p Packet) SetFlagPSH()       { p[13] |= FlagPSH }
+func (p Packet) SetFlagRST()       { p[13] |= FlagRST }
+func (p Packet) SetFlagSYN()       { p[13] |= FlagSYN }
+func (p Packet) SetFlagFIN()       { p[13] |= FlagFIN }
--- reader.go
+++ reader.go
@@ -3,42 +3,13 @@ package tcp
 
 import "encoding/binary"
 
-func (p Packet) SourcePort() uint16 {
-	return binary.BigEndian.Uint16(p[0:])
-}
-
-func (p Packet) DestinationPort() uint16 {
-	return binary.BigEndian.Uint16(p[2:])
-}
-
-func (p Packet) SequenceNumber() uint32 {
-	return binary.BigEndian.Uint32(p[4:])
-}
-
-func (p Packet) AckNumber() uint32 {
-	return binary.BigEndian.Uint32(p[8:])
-}
-
-func (p Packet) DataOffset() uint8 {
-	return (p[12] >> 4) * 4
-}
-
-func (p Packet) Window() uint16 {
-	return binary.BigEndian.Uint16(p[14:])
-}
-
-func (p Packet) GetChecksum() uint16 {
-	return binary.BigEndian.Uint16(p[16:])
-}
-
-func (p Packet) UrgentPointer() uint16 {
-	return binary.BigEndian.Uint16(p[18:])
-}
-
-func (p Packet) Options() []byte {
-	return p[20:p.DataOffset()]
-}
-
-func (p Packet) Payload() []byte {
-	return p[p.DataOffset():]
-}
+func (p Packet) SourcePort() uint16      { return binary.BigEndian.Uint16(p[0:]) }
+func (p Packet) DestinationPort() uint16 { return binary.BigEndian.Uint16(p[2:]) }
+func (p Packet) SequenceNumber() uint32  { return binary.BigEndian.Uint32(p[4:]) }
+func (p Packet) AckNumber() uint32       { return binary.BigEndian.Uint32(p[8:]) }
+func (p Packet) DataOffset() uint8       { return (p[12] >> 4) * 4 }
+func (p Packet) Window() uint16          { return binary.BigEndian.Uint16(p[14:]) }
+func (p Packet) GetChecksum() uint16     { return binary.BigEndian.Uint16(p[16:]) }
+func (p Packet) UrgentPointer() uint16   { return binary.BigEndian.Uint16(p[18:]) }
+func (p Packet) Options() []byte         { return p[20:p.DataOffset()] }
+func (p Packet) Payload() []byte         { return p[p.DataOffset():] }
